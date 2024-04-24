afa20acd0170be94d3aee28df854acedefc905e8
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-04-24 15:51:33 -0400

    lab complete

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name=first_name, last_name=last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge:str):
+        self.knowledge.append(new_knowledge)
